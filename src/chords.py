32fbacaae110c187900a44a6ea5e18cb68e2507e
bminahan73 <ben.minahan@egov.com>
2018-10-26 16:33:03 -0500

    adjusted chord definition

--- src/chords.py
+++ src/chords.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-#builds on chromatic scale given root note
-#TODO: implement extended chords. jno idea HTF to do that in this model...
+#lists represent half-step offsets from root note
 MAJOR = [0,4,7]
 MINOR = [0,3,7]
 DIMINISHED = [0,3,6]
